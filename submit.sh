5a366aba9bbdc4fd814c9987502d0c870461d8e4
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2023-12-04 12:28:52 -0500

    change test noises

--- submit.sh
+++ submit.sh
@@ -1,5 +1,5 @@
 full_noises=("saturate.npy" "spatter.npy" "gaussian_blue.npy" "speckle_noise.npy" "jpeg_compression.npy" "pixelate.npy" "elastic_transform.npy" "contrast.npy" "brightness.npy" "fog.npy" "frost.npy" "snow.npy" "zoom_blur.npy" "motion_blur.npy" "defocus.npy" "impulse_noise.npy" "shot_noise.npy" "gaussian_noise.npy")
-test_noises=("shot_noise.npy" "gaussian_noise.npy")
+test_noises=("spatter.npy" "brightness.npy")
 
 if [[ "$1" == "test" ]]; then
     noises=("${test_noises[@]}")
