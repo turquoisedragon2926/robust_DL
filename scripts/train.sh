2c52cf3124082c76a81406da5e5232dbc9f8568b
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2023-12-12 00:10:20 -0500

    train scripts

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,8 +1,8 @@
 # severities=(0.05 0.1 0.25 0.5 0.75 1)
 # train_noises=("gaussian" "uniform" "shot" "blur" "random")
 
-severities=(0.05 0.1)
-train_noises=("gaussian" "random")
+severities=(0.05)
+train_noises=("gaussian")
 
 for train_noise in "${train_noises[@]}"; do
     for severity in "${severities[@]}"; do
