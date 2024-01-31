089f7e797cf1cb8eecb8a00ceb18d07acb79197f
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-01-31 14:02:32 -0500

    lrs

--- plotting/ce_lr.py
+++ plotting/ce_lr.py
@@ -34,7 +34,7 @@ def main():
 
     train_noise = "gaussian"
     eval_noises = ["none", "adversarial", "saturate.npy", "spatter.npy", "gaussian_blur.npy", "speckle_noise.npy", "jpeg_compression.npy", "pixelate.npy", "elastic_transform.npy", "contrast.npy", "brightness.npy", "fog.npy", "frost.npy", "snow.npy", "zoom_blur.npy", "motion_blur.npy", "defocus_blur.npy", "impulse_noise.npy", "shot_noise.npy", "gaussian_noise.npy"]
-    lrs = [0.005, 0.01]
+    lrs = [0.005, 0.01, 0.03, 0.05]
     alphas = [2.0]
     alpha = alphas[0]
 
