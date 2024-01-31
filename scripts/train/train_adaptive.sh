db3da7cc9b84537455d0bb8f64ebcc1c062ec36c
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-01-31 00:43:33 -0500

    print results

--- scripts/train/train_adaptive.sh
+++ scripts/train/train_adaptive.sh
@@ -1,17 +1,20 @@
-alphas=(0.1 0.25 0.5 1 2 3)
+severities=(0.05 0.1 0.25 0.5 0.75 1)
 
-for alpha in "${alphas[@]}"; do
+for severity in "${severities[@]}"; do
     bash scripts/experiment.sh \
         --mode_type train \
         --attack_type identity \
         --train_dataset cifar10 \
         --eval_dataset cifar10C \
         --model_type resnet18 \
-        --loss_type trades \
+        --loss_type adaptive \
+        --train_noise uniform \
         --eval_noise gaussian_noise.npy \
         --epochs 100 \
         --valid_size 0.2 \
         --eval_interval 1 \
-        --lr 0.01 \
-        --alpha $alpha
+        --severity $severity \
+        --w_noise 0.1 \
+        --lr 0.03 \
+        --tau1 10
 done
---
+++ scripts/train/train_lr_trades.sh
@@ -0,0 +1,20 @@
+alphas=(0.1 0.25 1 3)
+lrs=(0.005 0.01 0.03 0.05)
+
+for lr in "${lrs[@]}"; do
+    for alpha in "${alphas[@]}"; do
+        bash scripts/experiment.sh \
+            --mode_type train \
+            --attack_type identity \
+            --train_dataset cifar10 \
+            --eval_dataset cifar10C \
+            --model_type resnet18 \
+            --loss_type trades \
+            --eval_noise gaussian_noise.npy \
+            --epochs 100 \
+            --valid_size 0.2 \
+            --eval_interval 1 \
+            --lr $lr \
+            --alpha $alpha
+    done
+done
