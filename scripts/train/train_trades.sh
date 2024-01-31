40b894023000f876814c4bc3ccf760d78472af00
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-01-30 22:07:59 -0500

    more lr

--- scripts/train/train_trades.sh
+++ scripts/train/train_trades.sh
@@ -18,3 +18,17 @@ for lr in "${lrs[@]}"; do
             --alpha $alpha
     done
 done
+
+        python3 main.py \
+            --mode_type train \
+            --attack_type identity \
+            --train_dataset cifar10 \
+            --eval_dataset cifar10C \
+            --model_type resnet18 \
+            --loss_type trades \
+            --eval_noise gaussian_noise.npy \
+            --epochs 100 \
+            --valid_size 0.2 \
+            --eval_interval 1 \
+            --lr 0.03 \
+            --alpha 0.25
