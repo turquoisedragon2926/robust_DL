3d78293c45c779c8ce841203dbf2bafba2004f1a
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-01-31 15:13:28 -0500

    print loss ce

---
+++ scripts/plot/print_ce.sh
@@ -0,0 +1,14 @@
+python3 plotting/print_eval.py \
+    --mode_type train \
+    --attack_type identity \
+    --model_type resnet18 \
+    --train_dataset cifar10 \
+    --eval_dataset cifar10C \
+    --loss_type ce \
+    --eval_noise gaussian_noise.npy \
+    --epochs 100 \
+    --valid_size 0.2 \
+    --eval_interval 1 \
+    --w_noise 0.1 \
+    --lr 0.01 \
+    --tau1 10
