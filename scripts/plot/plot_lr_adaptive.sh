93983f12df18dd021030199dcefdbe795dc0f527
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-01-30 21:34:54 -0500

    adaptive

---
+++ scripts/plot/plot_lr_adaptive.sh
@@ -0,0 +1,11 @@
+python3 plotting/adaptive_lr_alpha.py \
+    --mode_type train \
+    --attack_type identity \
+    --train_dataset cifar10 \
+    --eval_dataset cifar10C \
+    --model_type resnet18 \
+    --loss_type adaptive \
+    --eval_noise gaussian_noise.npy \
+    --epochs 100 \
+    --valid_size 0.2 \
+    --eval_interval 1 \
