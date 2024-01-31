25061371202f7828a3f53044f8e3ccb79730c2b4
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-01-31 14:45:13 -0500

    fix for all params

--- plotting/all_losses.py
+++ plotting/all_losses.py
@@ -34,8 +34,8 @@ def main():
 
     eval_noises = ["none", "adversarial", "saturate.npy", "spatter.npy", "gaussian_blur.npy", "speckle_noise.npy", "jpeg_compression.npy", "pixelate.npy", "elastic_transform.npy", "contrast.npy", "brightness.npy", "fog.npy", "frost.npy", "snow.npy", "zoom_blur.npy", "motion_blur.npy", "defocus_blur.npy", "impulse_noise.npy", "shot_noise.npy", "gaussian_noise.npy"]
 
-    w_noises = [0.05, 0.1] #, 0.25, 0.5, 0.75, 1.0]
-    alphas = [0.1, 0.25] #, 0.5, 1.0, 2.0, 3.0]
+    w_noises = [0.05, 0.1, 0.25, 0.5, 0.75, 1.0]
+    alphas = [0.1, 0.25, 0.5, 1.0, 2.0, 3.0]
 
     if args.model_type == 'alexnet':
         model = AlexNet().to(device)
@@ -196,7 +196,7 @@ def main():
     print(w_noise_accuracies)
 
     # Call the new plotting function
-    plotter.plot_accuracy_vs_parameters(alpha_accuracies, w_noise_accuracies, alphas, w_noises)
+    plotter.plot_accuracy_vs_parameters(alpha_accuracies, w_noise_accuracies, alphas, w_noises, plot_name=f"{configuration.id}_accuracy_vs_parameters.png")
 
 if __name__ == "__main__":
     main()
