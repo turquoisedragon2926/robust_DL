41da6530b12cca7f579c765325b091ac14bc8829
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-01-31 23:55:27 -0500

    new training

--- scripts/train/train_severity_trainnoise.sh
+++ scripts/train/train_severity_trainnoise.sh
@@ -1,5 +1,5 @@
 severities=(0.05 0.1 0.25 0.5 0.75 1)
-train_noises=("blur" "uniform") # ("gaussian" "uniform" "shot" "dynamicBlur" "random")
+train_noises=("uniform") # ("blur" "gaussian" "uniform" "shot" "dynamicBlur" "random")
 
 for train_noise in "${train_noises[@]}"; do
     for severity in "${severities[@]}"; do
