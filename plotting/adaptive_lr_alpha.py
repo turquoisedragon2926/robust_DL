c91ec4d89c65246b967192ef3d1fece6ae98db2d
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-04-17 00:15:27 -0400

    force test

--- plotting/adaptive_lr_alpha.py
+++ plotting/adaptive_lr_alpha.py
@@ -113,7 +113,7 @@ def main():
                 data.attack_loader = attack_loader
                 configuration.data = data
 
-                robustness_accuracy = load_from_key(robustness_accuracy_path, configuration.id)
+                robustness_accuracy = None # load_from_key(robustness_accuracy_path, configuration.id)
                 if robustness_accuracy is None:
                     robustness_accuracy = robust_accuracy(configuration, device)
                     save_to_key(robustness_accuracy_path, configuration.id, robustness_accuracy)
