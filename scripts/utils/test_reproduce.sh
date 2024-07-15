9f7f7dd9d8444850d233bb62338951d45a76d670
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-07-15 02:05:48 -0400

    test single epoch

--- scripts/utils/test_reproduce.sh
+++ scripts/utils/test_reproduce.sh
@@ -1,23 +1,26 @@
-#!/bin/bash
+models=("resnet18")
+losses=("adaptive")
 
-lrs=(0.005)  
-severities=(0.05)  
+# module load conda
+# conda activate robust_DL
+# module load pytorch/2.0.1
 
-for lr in "${lrs[@]}"; do
-    for severity in "${severities[@]}"; do
-        bash scripts/experiment.sh \
+for loss in "${losses[@]}"; do
+    for model in "${models[@]}"; do
+        python3 main.py \
             --mode_type train \
             --attack_type identity \
-            --train_dataset cifar10 \
-            --eval_dataset cifar10C \
-            --model_type resnet18 \
-            --loss_type adaptive \
-            --train_noise uniform \
+            --model_type $model \
+            --train_dataset imagenet \
+            --eval_dataset imagenetC \
+            --loss_type $loss \
+            --train_noise blur \
             --eval_noise gaussian_noise.npy \
             --epochs 1 \
             --valid_size 0.2 \
             --eval_interval 1 \
-            --severity $severity \
-            --lr $lr
+            --severity 0.05 \
+            --w_noise 0.1 \
+            --tau1 10
     done
 done
