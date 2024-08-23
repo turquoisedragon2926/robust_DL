915bdd090111b422f6d886c3e6832738f05cb73d
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-08-23 09:58:49 -0400

    plotting fixed sample

---
+++ scripts/plot/plot_sample.sh
@@ -0,0 +1,13 @@
+python3 plotting/adaptive_lr_alpha.py \
+    --mode_type train \
+    --attack_type identity \
+    --train_dataset cifar10 \
+    --eval_dataset cifar10C \
+    --train_noise uniform \
+    --model_type resnet18 \
+    --loss_type adaptive \
+    --eval_noise gaussian_noise.npy \
+    --epochs 100 \
+    --valid_size 0.2 \
+    --eval_interval 1 \
+    --num_samples 2
---
+++ scripts/train/train_sample.sh
@@ -0,0 +1,17 @@
+bash scripts/experiment.sh \
+    --mode_type train \
+    --attack_type identity \
+    --train_dataset cifar10 \
+    --eval_dataset cifar10C \
+    --model_type resnet18 \
+    --loss_type adaptive \
+    --train_noise uniform \
+    --eval_noise gaussian_noise.npy \
+    --epochs 100 \
+    --valid_size 0.2 \
+    --eval_interval 1 \
+    --severity 0.5 \
+    --w_noise 0.1 \
+    --lr 0.05 \
+    --tau1 10 \
+    --num_samples 2
