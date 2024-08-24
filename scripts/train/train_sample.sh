84d606f22696c9f40d28744f3e6cb34d3dacb881
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-08-24 10:15:23 -0400

    train samples

--- scripts/train/train_sample.sh
+++ scripts/train/train_sample.sh
@@ -5,13 +5,31 @@ bash scripts/experiment.sh \
     --eval_dataset cifar10C \
     --model_type resnet18 \
     --loss_type adaptive \
-    --train_noise uniform \
+    --train_noise gaussian \
     --eval_noise gaussian_noise.npy \
     --epochs 100 \
     --valid_size 0.2 \
     --eval_interval 1 \
-    --severity 0.5 \
+    --severity 0.05 \
     --w_noise 0.1 \
     --lr 0.05 \
     --tau1 10 \
-    --num_samples 2
+    --num_samples 1
+    
+bash scripts/experiment.sh \
+    --mode_type train \
+    --attack_type identity \
+    --train_dataset cifar10 \
+    --eval_dataset cifar10C \
+    --model_type alexnet \
+    --loss_type adaptive \
+    --train_noise gaussian \
+    --eval_noise gaussian_noise.npy \
+    --epochs 100 \
+    --valid_size 0.2 \
+    --eval_interval 1 \
+    --severity 0.05 \
+    --w_noise 0.1 \
+    --lr 0.05 \
+    --tau1 10 \
+    --num_samples 1
