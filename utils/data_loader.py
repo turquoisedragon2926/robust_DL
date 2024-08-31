54ea843434bf17d28fdcaab84a9552db6f2dc232
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-08-31 17:00:49 -0400

    imageent loading

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -3,9 +3,48 @@ import torch
 import numpy as np
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, SubsetRandomSampler
-# from .components import AttackDataset
+# from .components import AttackDataset, ImageNetKaggle
 
+import json
+from PIL import Image
 from torch.utils.data import Dataset
+
+class ImageNetKaggle(Dataset):
+    def __init__(self, root, split, transform=None):
+        self.samples = []
+        self.targets = []
+        self.transform = transform
+        self.syn_to_class = {}
+        with open(os.path.join(root, "imagenet_class_index.json"), "rb") as f:
+                    json_file = json.load(f)
+                    for class_id, v in json_file.items():
+                        self.syn_to_class[v[0]] = int(class_id)
+        with open(os.path.join(root, "ILSVRC2012_val_labels.json"), "rb") as f:
+                    self.val_to_syn = json.load(f)
+        samples_dir = os.path.join(root, "ILSVRC/Data/CLS-LOC", split)
+        for entry in os.listdir(samples_dir):
+            if split == "train":
+                syn_id = entry
+                target = self.syn_to_class[syn_id]
+                syn_folder = os.path.join(samples_dir, syn_id)
+                for sample in os.listdir(syn_folder):
+                    sample_path = os.path.join(syn_folder, sample)
+                    self.samples.append(sample_path)
+                    self.targets.append(target)
+            elif split == "val":
+                syn_id = self.val_to_syn[entry]
+                target = self.syn_to_class[syn_id]
+                sample_path = os.path.join(samples_dir, entry)
+                self.samples.append(sample_path)
+                self.targets.append(target)
+    def __len__(self):
+            return len(self.samples)
+    def __getitem__(self, idx):
+            x = Image.open(self.samples[idx]).convert("RGB")
+            if self.transform:
+                x = self.transform(x)
+            return x, self.targets[idx]
+        
 class AttackDataset(Dataset):
     def __init__(self, data, labels, transform=None):
         self.data = data
@@ -110,9 +149,10 @@ class DataLoaderFactory:
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
 
-        images = np.load(os.path.join('data/ImageNet-C', eval_noise))
-        labels = np.load('data/ImageNet-C/labels.npy')
-        imagenetc_dataset = AttackDataset(data=images, labels=labels, transform=transform_imagenetc)
+        # images = np.load(os.path.join('data/ImageNet-C', eval_noise))
+        # labels = np.load('data/ImageNet-C/labels.npy')
+        # imagenetc_dataset = AttackDataset(data=images, labels=labels, transform=transform_imagenetc)
+        imagenetc_dataset = ImageNetKaggle(os.path.join(self.root, "imagenet"), 'val', transform=transform_imagenetc)
         imagenetc_attack_loader = DataLoader(imagenetc_dataset, batch_size=self.batch_size, shuffle=False)
 
         return imagenetc_attack_loader
@@ -135,9 +175,9 @@ class DataLoaderFactory:
         ])
 
         # Load datasets, assuming you have ImageNet dataset in the specified path
-        trainset = datasets.ImageNet(root=os.path.join(self.root), split='train', transform=transform_train)
-        validset = datasets.ImageNet(root=os.path.join(self.root), split='train', transform=transform_train)
-        testset = datasets.ImageNet(root=os.path.join(self.root), split='val', transform=transform_test)
+        trainset = ImageNetKaggle(os.path.join(self.root, "imagenet"), 'train', transform=transform_train)
+        validset = ImageNetKaggle(os.path.join(self.root, "imagenet"), 'val', transform=transform_train)
+        testset = ImageNetKaggle(os.path.join(self.root, "imagenet"), 'val', transform=transform_test)
 
         return trainset, validset, testset
 
