001ed12feee584046ead4a4b2975ec58144a48f2
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-08-31 19:56:30 -0400

    move stuff

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -3,67 +3,7 @@ import torch
 import numpy as np
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, SubsetRandomSampler
-# from .components import AttackDataset, ImageNetKaggle
-
-import json
-from PIL import Image
-from torch.utils.data import Dataset
-
-class ImageNetKaggle(Dataset):
-    def __init__(self, root, split, transform=None, n_classes=10):
-        self.samples = []
-        self.targets = []
-        self.transform = transform
-        self.syn_to_class = {}
-        self.n_classes = n_classes
-
-        with open(os.path.join(root, "imagenet_class_index.json"), "rb") as f:
-                    json_file = json.load(f)
-                    for class_id, v in json_file.items():
-                        self.syn_to_class[v[0]] = int(class_id)
-        with open(os.path.join(root, "ILSVRC2012_val_labels.json"), "rb") as f:
-                    self.val_to_syn = json.load(f)
-        samples_dir = os.path.join(root, "ILSVRC/Data/CLS-LOC", split)
-        for entry in os.listdir(samples_dir):
-            if split == "train":
-                syn_id = entry
-                target = self.syn_to_class[syn_id]
-                if target < self.n_classes:
-                    syn_folder = os.path.join(samples_dir, syn_id)
-                    for sample in os.listdir(syn_folder):
-                        sample_path = os.path.join(syn_folder, sample)
-                        self.samples.append(sample_path)
-                        self.targets.append(target)
-            elif split == "val":
-                syn_id = self.val_to_syn[entry]
-                target = self.syn_to_class[syn_id]
-                if target < self.n_classes:
-                    sample_path = os.path.join(samples_dir, entry)
-                    self.samples.append(sample_path)
-                    self.targets.append(target)
-    def __len__(self):
-            return len(self.samples)
-    def __getitem__(self, idx):
-            x = Image.open(self.samples[idx]).convert("RGB")
-            if self.transform:
-                x = self.transform(x)
-            return x, self.targets[idx]
-        
-class AttackDataset(Dataset):
-    def __init__(self, data, labels, transform=None):
-        self.data = data
-        self.labels = labels
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        image = self.data[idx]
-        label = self.labels[idx]
-        if self.transform:
-            image = self.transform(image)
-        return image, label
+from .components import AttackDataset, ImageNetKaggle
 
 class DataLoaderFactory:
     def __init__(self, root, valid_size, train_dataset, eval_dataset, batch_size=128, num_workers=1, pin_memory=True):
