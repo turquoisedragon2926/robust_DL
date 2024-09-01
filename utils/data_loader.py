86cfbd4206984f41cd941d1d16d2fb96e781959d
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-09-01 01:25:43 -0400

    grid plot

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -3,7 +3,101 @@ import torch
 import numpy as np
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, SubsetRandomSampler
-from .components import AttackDataset, ImageNetKaggle
+# from .components import AttackDataset, ImageNetKaggle
+
+from torch.utils.data import Dataset
+import json
+from PIL import Image
+from torch.utils.data import Dataset
+import os
+
+class Data:
+  def __init__(self, train_loader, valid_loader, test_loader, attack_loader):
+    self.train_loader = train_loader
+    self.valid_loader = valid_loader
+    self.test_loader = test_loader
+    self.attack_loader = attack_loader
+
+class Configuration:
+  def __init__(self, data, model, optimizer, loss_fn, attack, id=None):
+    self.data = data
+    self.model = model
+    self.loss_fn = loss_fn
+    self.attack = attack
+    self.optimizer = optimizer
+
+    self.id = id
+
+  def getId(self):
+    return self.id
+  
+class AttackDataset(Dataset):
+    def __init__(self, data, labels, transform=None):
+        self.data = data
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        image = self.data[idx]
+        label = self.labels[idx]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+class ImageNetKaggle(Dataset):
+    def __init__(self, root, split, transform=None, n_classes=10, max_samples_per_class=3):
+        self.samples = []
+        self.targets = []
+        self.transform = transform
+        self.syn_to_class = {}
+        self.n_classes = n_classes
+        self.max_samples_per_class = max_samples_per_class
+        
+        with open(os.path.join(root, "imagenet_class_index.json"), "rb") as f:
+            json_file = json.load(f)
+            for class_id, v in json_file.items():
+                self.syn_to_class[v[0]] = int(class_id)
+        with open(os.path.join(root, "ILSVRC2012_val_labels.json"), "rb") as f:
+            self.val_to_syn = json.load(f)
+
+        samples_dir = os.path.join(root, "ILSVRC/Data/CLS-LOC", split)
+        class_sample_counts = {i: 0 for i in range(n_classes)}  
+        
+        for entry in os.listdir(samples_dir):
+            if split == "train":
+                syn_id = entry
+                target = self.syn_to_class[syn_id]
+                if target < self.n_classes:
+                    syn_folder = os.path.join(samples_dir, syn_id)
+                    for sample in os.listdir(syn_folder):
+                        if self.max_samples_per_class and class_sample_counts[target] >= self.max_samples_per_class:
+                            continue
+                        sample_path = os.path.join(syn_folder, sample)
+                        self.samples.append(sample_path)
+                        self.targets.append(target)
+                        class_sample_counts[target] += 1
+            elif split == "val":
+                syn_id = self.val_to_syn[entry]
+                target = self.syn_to_class[syn_id]
+                if target < self.n_classes:
+                    if self.max_samples_per_class and class_sample_counts[target] >= self.max_samples_per_class:
+                        continue
+                    sample_path = os.path.join(samples_dir, entry)
+                    self.samples.append(sample_path)
+                    self.targets.append(target)
+                    class_sample_counts[target] += 1
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        x = Image.open(self.samples[idx]).convert("RGB")
+        if self.transform:
+            x = self.transform(x)
+        return x, self.targets[idx]
+
 
 class DataLoaderFactory:
     def __init__(self, root, valid_size, train_dataset, eval_dataset, batch_size=128, num_workers=1, pin_memory=True):
@@ -153,7 +247,9 @@ if __name__ == "__main__":
     images_train, labels_train = next(iter(train_loader))
 
     # Plot the images in a 2x2 grid
-    plot_images_grid(images_train, labels_train, nrow=2, ncol=2, path="results/plots/clean.png")
+    plot_images_grid(images_train, labels_train, nrow=2, ncol=2, path="trainclean.png")
+    plot_images_grid(valid_loader, labels_train, nrow=2, ncol=2, path="validclean.png")
+    plot_images_grid(test_loader, labels_train, nrow=2, ncol=2, path="testclean.png")
 
     # Get ImageNet-C attack loader (replace 'noise.npy' with the actual noise file you have)
     imagenetc_attack_loader = factory.get_attack_loader(eval_noise='defocus.npy')
@@ -162,4 +258,4 @@ if __name__ == "__main__":
     images_attack, labels_attack = next(iter(imagenetc_attack_loader))
 
     # Plot the images in a 2x2 grid
-    plot_images_grid(images_attack, labels_attack, nrow=2, ncol=2, path="results/plots/cleanc.png")
+    plot_images_grid(images_attack, labels_attack, nrow=2, ncol=2, path="cleanc.png")
