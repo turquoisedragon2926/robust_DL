5a7bbbfd0a2ba77da937777bb3f046d107317d57
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-09-15 10:31:46 -0400

    add aug to params

--- utils/utils.py
+++ utils/utils.py
@@ -9,7 +9,7 @@ def parse_args():
     parser.add_argument('--mode_type', type=str, default="train", choices=['eval', 'train'], help='Mode type: train or eval only')
     parser.add_argument('--attack_type', type=str, default="identity", choices=['identity', 'pgd'], help='Attack type: identity or pgd only')
     parser.add_argument('--model_type', type=str, default="alexnet", choices=['alexnet', 'resnet18'], help='Model type: alexnet')
-    parser.add_argument('--train_dataset', type=str, default="cifar10", choices=['cifar10', 'imagenet'], help='Training Dataset type (default: cifar10)')
+    parser.add_argument('--train_dataset', type=str, default="cifar10", choices=['cifar10', 'augcifar10', 'imagenet'], help='Training Dataset type (default: cifar10)')
     parser.add_argument('--eval_dataset', type=str, default="cifar10C", choices=['cifar10C', 'imagenetC'], help='Evaluation Dataset type (default: cifar10C)')
     parser.add_argument('--loss_type', type=str, default="adaptive", choices=['trades', 'adaptive', 'ce', 'adversarial', 'augmix'], help='Loss type: trades / custom / ce / adversarial')
     parser.add_argument('--train_noise', type=str, default='gaussian', choices=['gaussian', 'uniform', 'shot', 'blur', 'random', 'dynamicBlur'], help='Type of noise to use while training (default: gaussian)')
